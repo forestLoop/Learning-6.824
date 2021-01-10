f773de62e38d80d05807029e69a1a67332627c2c
Keith Null <keith1126@126.com>
2021-01-10 16:03:11 +0800

    Pass the first 2 test cases

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -2,13 +2,18 @@ package kvraft
 
 import (
 	"crypto/rand"
+	"log"
 	"math/big"
+	mathrand "math/rand"
+	"os"
 
 	"github.com/keithnull/Learning-6.824/src/labrpc"
 )
 
 type Clerk struct {
-	servers []*labrpc.ClientEnd
+	servers    []*labrpc.ClientEnd
+	lastLeader int // -1 for unknown
+	logger     *log.Logger
 	// You will have to modify this struct.
 }
 
@@ -20,10 +25,20 @@ func nrand() int64 {
 }
 
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
-	ck := new(Clerk)
-	ck.servers = servers
-	// You'll have to add code here.
+	ck := &Clerk{
+		servers:    servers,
+		lastLeader: -1,
+		logger:     log.New(os.Stdout, "[Client]", log.Ltime|log.Lmicroseconds),
+	}
+	muteLoggerIfUnset(ck.logger, "debug_clerk")
 	return ck
+}
+
+func (ck *Clerk) getLastLeader() int {
+	if ck.lastLeader != -1 {
+		return ck.lastLeader
+	}
+	return mathrand.Int() % len(ck.servers)
 }
 
 //
@@ -39,9 +54,26 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) Get(key string) string {
-
-	// You will have to modify this function.
-	return ""
+	args := GetArgs{
+		Key: key,
+	}
+	targetServer := ck.getLastLeader() - 1
+	for {
+		targetServer = (targetServer + 1) % len(ck.servers)
+		ck.logger.Printf("Try to contact server %v for Get: args = %v", targetServer, args)
+		reply := GetReply{}
+		ok := ck.servers[targetServer].Call("KVServer.Get", &args, &reply)
+		if !ok {
+			ck.logger.Printf("Server %v didn't respond for Get", targetServer)
+			continue
+		}
+		if reply.Err != OK {
+			ck.logger.Printf("Server %v responded with Error %v for Get", targetServer, reply.Err)
+			continue
+		}
+		ck.lastLeader = targetServer
+		return reply.Value
+	}
 }
 
 //
@@ -55,7 +87,28 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
-	// You will have to modify this function.
+	args := PutAppendArgs{
+		Key:   key,
+		Value: value,
+		Op:    op,
+	}
+	targetServer := ck.getLastLeader() - 1
+	for {
+		targetServer = (targetServer + 1) % len(ck.servers)
+		ck.logger.Printf("Try to contact server %v for PutAppend: args = %v", targetServer, args)
+		reply := PutAppendReply{}
+		ok := ck.servers[targetServer].Call("KVServer.PutAppend", &args, &reply)
+		if !ok {
+			ck.logger.Printf("Server %v didn't respond for PutAppend", targetServer)
+			continue
+		}
+		if reply.Err != OK {
+			ck.logger.Printf("Server %v responded with Error %v for PutAppend", targetServer, reply.Err)
+			continue
+		}
+		ck.lastLeader = targetServer
+		return
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
